f8ad1448e081ff656204548a7b3e5886bd2409ce
Oliver Chen <olichen@ucdavis.edu>
2020-05-22 12:00:08 -0700

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='LiquidHandler',
+      version='0.1',
+      description='Opentrons protocol generator for Eton Bioscience Inc.',
+      author='Oliver Chen',
+      author_email='olichen@ucdavis.edu',
+      url='https://github.com/olichen/liquidhandler',
+      packages=['liquidhandler.'],
+     )
