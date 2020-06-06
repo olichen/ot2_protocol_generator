4c68d94543fb2f2aa9489b07afb5634453155599
Oliver Chen <olichen@ucdavis.edu>
2020-06-06 15:35:29 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
-setup(name='LiquidHandler',
-      version='0.1',
-      description='Opentrons protocol generator for Eton Bioscience Inc.',
-      author='Oliver Chen',
-      author_email='olichen@ucdavis.edu',
-      url='https://github.com/olichen/liquidhandler',
-      packages=['liquidhandler.'],
-     )
+from setuptools import setup
+import os
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'LiquidHandler',
+    url = 'https://github.com/olichen/liquidhandler',
+    version = '0.1',
+    author = 'Oliver Chen',
+    author_email = 'olichen@ucdavis.edu',
+    description = 'Operntrons protocol generator for Eton Bioscience Inc.',
+    long_description = read('README'),
+    license = 'MIT',
+    packages = ['liquidhandler', 'tests'],
+    install_requires = ['opentrons']
+)
