68832fc93234fe15399fad9b62fd5f627e40de2d
Oliver Chen <olichen@ucdavis.edu>
2020-06-12 15:53:48 -0700

    rearranged data

--- liquidhandler/protocol_data.py
+++ liquidhandler/protocol_data.py
@@ -3,12 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class ProtocolData:
-    pipette_type: str
-    pipette_loc: str
     tip_rack_type: str
     tip_rack_loc: str
     src_plate_type: str
     src_plate_loc: str
     dest_plate_type: str
     dest_plate_loc: str
+    pipette_type: str
+    pipette_loc: str
     csv_file_loc: str
