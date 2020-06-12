57ba88c8404b8cf5e0385784f31ce3ec8da5e142
Oliver Chen <olichen@ucdavis.edu>
2020-06-12 15:54:12 -0700

    added dataclass to replace long list of data

--- liquidhandler/protocol_generator.py
+++ liquidhandler/protocol_generator.py
@@ -2,18 +2,12 @@ import tkinter as tk
 from tkinter import filedialog
 import csv_reader
 import output_writer
+import protocol_data
 
 
 class ProtocolGenerator:
-    def __init__(self):
-        self.tip_rack_type = 'geb_96_tiprack_10ul'
-        self.tip_rack_loc = 1
-        self.src_plate_type = 'appliedbiosystems_96_wellplate_100ul'
-        self.src_plate_loc = 2
-        self.dst_plate_type = 'appliedbiosystems_96_wellplate_100ul'
-        self.dst_plate_loc = 3
-        self.pipette_type = 'p10_single'
-        self.pipette_loc = 'right'
+    def __init__(self, data):
+        self.data = data
 
     def main(self):
         file_path = self.getCSVFile()
@@ -27,10 +21,10 @@ class ProtocolGenerator:
 
         with open(output_file, 'w') as f1:
             f1.write(ow.getHeader())
-            f1.write(ow.getTipRack(self.tip_rack_type, self.tip_rack_loc))
-            f1.write(ow.getSrcPlate(self.src_plate_type, self.src_plate_loc))
-            f1.write(ow.getDestPlate(self.dst_plate_type, self.dst_plate_loc))
-            f1.write(ow.getPipette(self.pipette_type, self.pipette_loc))
+            f1.write(ow.getTipRack(self.data.tip_rack_type, self.data.tip_rack_loc))
+            f1.write(ow.getSrcPlate(self.data.src_plate_type, self.data.src_plate_loc))
+            f1.write(ow.getDestPlate(self.data.dest_plate_type, self.data.dest_plate_loc))
+            f1.write(ow.getPipette(self.data.pipette_type, self.data.pipette_loc))
 
             for well, volume in csv_volumes:
                 f1.write(ow.getSingleTransfer(volume, well))
@@ -52,4 +46,15 @@ class ProtocolGenerator:
 
 
 if __name__ == "__main__":
-    ProtocolGenerator().main()
+    data = protocol_data.ProtocolData(
+        tip_rack_type='geb_96_tiprack_10ul',
+        tip_rack_loc='1',
+        src_plate_type='appliedbiosystems_96_wellplate_100ul',
+        src_plate_loc='2',
+        dest_plate_type='appliedbiosystems_96_wellplate_100ul',
+        dest_plate_loc='3',
+        pipette_type='p10_single',
+        pipette_loc='right',
+        csv_file_loc='placeholder')
+    pg = ProtocolGenerator(data)
+    pg.main()
