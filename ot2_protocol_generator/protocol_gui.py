a1d876ee24aa0e667ccd0f8e675069c1a954961d
Oliver Chen <olichen@ucdavis.edu>
2020-06-15 13:26:49 -0700

    replaced "b" with "placeholder"

--- ot2_protocol_generator/protocol_gui.py
+++ ot2_protocol_generator/protocol_gui.py
@@ -5,9 +5,9 @@ from tkinter import filedialog
 
 PIPETTE_TYPES = ('p10_single', 'p10_multi')
 PIPETTE_LOCS = ('right', 'left')
-TIP_RACK_TYPES = ('geb_96_tiprack_10ul', 'b')
+TIP_RACK_TYPES = ('geb_96_tiprack_10ul', 'placeholder')
 TIP_RACK_LOCS = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')
-PLATE_TYPES = ('appliedbiosystems_96_wellplate_100ul', 'b')
+PLATE_TYPES = ('appliedbiosystems_96_wellplate_100ul', 'placeholder')
 PLATE_LOCS = TIP_RACK_LOCS
 
 
