f71ec9f2be21578e425ce175480b31188e5fd0e5
Oliver Chen <olichen@ucdavis.edu>
2020-06-15 13:45:16 -0700

    updated test for protocol data; needs to be fixed more

--- tests/test_protocol_data.py
+++ tests/test_protocol_data.py
@@ -1,5 +1,6 @@
 from context import ot2_protocol_generator
 from ot2_protocol_generator import protocol_data
+from ot2_protocol_generator import config
 
 import unittest
 
@@ -7,44 +8,42 @@ import unittest
 class TestProtocolData(unittest.TestCase):
     def setUp(self):
         self.data = protocol_data.ProtocolData(
-                tip_rack_name='',
-                tip_rack_loc='',
-                src_plate_name='',
-                src_plate_loc='',
-                dest_plate_name='',
-                dest_plate_loc='',
-                pipette_name='',
+                tip_rack_name=config.TIP_RACK_NAMES[0],
+                tip_rack_loc=config.TIP_RACK_LOCS[0],
+                src_plate_name=config.PLATE_NAMES[0],
+                src_plate_loc=config.PLATE_LOCS[0],
+                dest_plate_name=config.PLATE_NAMES[0],
+                dest_plate_loc=config.PLATE_LOCS[0],
+                pipette_name=config.PIPETTE_NAMES[0],
+                pipette_loc=config.PIPETTE_LOCS[0],
                 pipette_type='',
-                pipette_loc='',
                 csv_file_loc='')
 
     def test_isValid(self):
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.tip_rack_name = 'a'
-        self.data.tip_rack_loc = '1'
+        self.data.pipette_type = 'right'
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.src_plate_name = 'b'
-        self.data.src_plate_loc = '2'
+        self.data.csv_file_loc = '/placeholder.py'
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.dest_plate_name = 'c'
-        self.data.dest_plate_loc = '3'
-        with self.assertRaises(ValueError):
-            self.data.isValid()
-
-        self.data.pipette_name = 'd'
-        self.data.pipette_type = 's'
-        self.data.pipette_loc = 'r'
-        with self.assertRaises(ValueError):
-            self.data.isValid()
-
-        self.data.csv_file_loc = 'f'
+        self.data.src_plate_loc = config.PLATE_LOCS[1]
+        self.data.dest_plate_loc = config.PLATE_LOCS[1]
         self.assertTrue(self.data.isValid())
+
+    def test_checkMissingInput(self):
+        with self.assertRaises(ValueError):
+            self.data.checkMissingInput()
+        #try:
+        #    eight_transfer.EightTransfer(self.volumedict)
+        #except Exception:
+        #    self.fail('Unexpected exception')
+
+
 
 
 if __name__ == '__main__':
