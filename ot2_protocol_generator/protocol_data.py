d02d171935adbdf25137cb9a0c43638288a17bb9
Oliver Chen <olichen@ucdavis.edu>
2020-06-15 13:27:17 -0700

    updated protocol data to also check and make sure all the plates are in different locations

--- ot2_protocol_generator/protocol_data.py
+++ ot2_protocol_generator/protocol_data.py
@@ -17,16 +17,31 @@ class ProtocolData:
 
     # Check to make sure there is input data for every variable
     def isValid(self):
+        self.checkForMissingInput()
+        self.checkForDifferentPlateLocations()
+
+        return True
+
+    def checkForMissingInput(self):
         missing_input = []
         for key, value in self.__dict__.items():
             if not value:
                 missing_input.append(key)
 
         if missing_input:
-            error_message = 'Missing input for:'
+            err_str = 'Missing input for:'
             for value in missing_input:
-                error_message += ' '
-                error_message += value
-            raise ValueError(error_message)
+                err_str += ' '
+                err_str += value
+            raise ValueError(err_str)
 
-        return True
+    def checkForDifferentPlateLocations(self):
+        if self.tip_rack_loc == self.src_plate_loc:
+            err_str = 'Tip rack and source plate are in the same location'
+            raise ValueError(err_str)
+        if self.tip_rack_loc == self.dest_plate_loc:
+            err_str = 'Tip rack and destination plate are in the same location'
+            raise ValueError(err_str)
+        if self.src_plate_loc == self.dest_plate_loc:
+            err_str = 'Source and destination plate are in the same location'
+            raise ValueError(err_str)
