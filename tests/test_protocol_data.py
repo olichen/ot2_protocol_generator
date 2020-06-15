59dcbf7e772192e328140eb7e41b5ffe1b67bbce
Oliver Chen <olichen@ucdavis.edu>
2020-06-15 13:20:37 -0700

    updated protocol data test

--- tests/test_protocol_data.py
+++ tests/test_protocol_data.py
@@ -7,36 +7,38 @@ import unittest
 class TestProtocolData(unittest.TestCase):
     def setUp(self):
         self.data = protocol_data.ProtocolData(
-            tip_rack_type='',
-            tip_rack_loc='',
-            src_plate_type='',
-            src_plate_loc='',
-            dest_plate_type='',
-            dest_plate_loc='',
-            pipette_type='',
-            pipette_loc='',
-            csv_file_loc='')
+                tip_rack_name='',
+                tip_rack_loc='',
+                src_plate_name='',
+                src_plate_loc='',
+                dest_plate_name='',
+                dest_plate_loc='',
+                pipette_name='',
+                pipette_type='',
+                pipette_loc='',
+                csv_file_loc='')
 
     def test_isValid(self):
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.tip_rack_type = 'a'
+        self.data.tip_rack_name = 'a'
         self.data.tip_rack_loc = '1'
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.src_plate_type = 'b'
+        self.data.src_plate_name = 'b'
         self.data.src_plate_loc = '2'
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.dest_plate_type = 'c'
+        self.data.dest_plate_name = 'c'
         self.data.dest_plate_loc = '3'
         with self.assertRaises(ValueError):
             self.data.isValid()
 
-        self.data.pipette_type = 'd'
+        self.data.pipette_name = 'd'
+        self.data.pipette_type = 's'
         self.data.pipette_loc = 'r'
         with self.assertRaises(ValueError):
             self.data.isValid()
