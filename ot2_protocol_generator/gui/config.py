784278c1d9a206019646abbf5a52cdc767ec6a86
Oliver Chen <olichen@ucdavis.edu>
2020-07-02 11:12:36 -0700

    updated config file

--- ot2_protocol_generator/gui/config.py
+++ ot2_protocol_generator/gui/config.py
@@ -1,6 +1,6 @@
-TIP_RACK_NAMES = ('geb_96_tiprack_10ul', 'placeholder')
-TIP_RACK_LOCS = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')
-PLATE_NAMES = ('appliedbiosystems_96_wellplate_100ul', 'placeholder')
+TIP_RACK_NAMES = ['geb_96_tiprack_10ul']
+TIP_RACK_LOCS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+PLATE_NAMES = ['appliedbiosystems_96_wellplate_100ul']
 PLATE_LOCS = TIP_RACK_LOCS
-PIPETTE_NAMES = ('p10_single', 'p10_multi')
-PIPETTE_LOCS = ('right', 'left')
+PIPETTE_NAMES = ['p10_single', 'p10_multi']
+PIPETTE_LOCS = ['right', 'left']
