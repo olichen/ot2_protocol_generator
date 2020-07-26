7acee5eff5a18bfe1039325b47e3d060ce743856
Oliver Chen <olichen@ucdavis.edu>
2020-07-26 15:30:24 -0700

    moved a lot of code into a separate class

---
+++ ot2_protocol_generator/helpers/menu_helper.py
@@ -0,0 +1,74 @@
+from tkinter import filedialog
+from tkinter import messagebox
+from ot2_protocol_generator import protocol_writer
+import webbrowser
+import os
+from platform import system
+import subprocess
+import logging
+from . import log_helper
+from ot2_protocol_generator.gui import config
+import traceback
+
+
+class MenuHelper:
+    def __init__(self):
+        # Initialize log handler
+        self.lh = log_helper.LogHandler()
+        logger = logging.getLogger()
+        logger.addHandler(self.lh)
+
+    # Outputs the protocol to a file
+    def save(self, parent, *data):
+        pw = protocol_writer.ProtocolWriter()
+
+        try:
+            # Read in data from each input panel. Throws an exception if
+            # invalid data is encountered
+            print(data)
+            for d in data:
+                pw.addData(d)
+
+            if ofile := filedialog.asksaveasfilename(title='Save Protocol'):
+                pw.saveOutput(ofile)
+
+                if self.lh.text:
+                    log_helper.WarningMessageBox(parent, self.lh.text)
+                parent.destroy()
+            else:
+                self.lh.clear()
+                parent.focus()
+        except Exception as e:
+            traceback.print_exc()
+            messagebox.showerror(title='Error', message=e)
+            self.lh.clear()
+            parent.focus()
+
+    # Open the labware.ini file for editing
+    def editLabware(self):
+        if not os.path.exists('./labware.ini'):
+            cfg = config.Configuration('./labware.ini')
+            cfg.writeFile('./labware.ini')
+
+        OS = system().lower()
+        if 'windows' in OS:
+            opener = 'start'
+        elif 'osx' in OS or 'darwin' in OS:
+            opener = 'open'
+        else:
+            opener = 'xdg-open'
+        subprocess.run(opener + ' labware.ini', shell=True)
+        msg = 'Please restart the protocol generator after editing any labware'
+        messagebox.showinfo(title='Labware', message=msg)
+
+    # Open the home page on github
+    def help(self):
+        url = 'https://github.com/olichen/ot2_protocol_generator#readme'
+        webbrowser.open(url)
+
+    # Pop open an about box
+    def about(self):
+        msg = ('Version 1.0\n'
+               'Copyright (c) 2020 Oliver Chen\n'
+               'https://github.com/olichen/ot2_protocol_generator')
+        messagebox.showinfo(title='About', message=msg)
--- ot2_protocol_generator/protocol_generator.py
+++ ot2_protocol_generator/protocol_generator.py
@@ -1,18 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
-from . import protocol_writer
 from .gui import pipette_input_panel
 from .gui import plate_input_panel
-from .gui import config
-import logging
-from .helpers import log_helper
-from platform import system
-import traceback
-import webbrowser
-import subprocess
-import os
+from .helpers import menu_helper
 
 
 class ProtocolGenerator:
@@ -21,18 +11,18 @@ class ProtocolGenerator:
         self.window = tk.Tk()
         self.window.title('Protocol Generator')
         self.window.resizable(False, False)
+
+        # Initialize menu bar
+        self.mh = menu_helper.MenuHelper()
         self.addMenubar()
 
         # Initialize the first input panel
         self.input_panels = []
         self.addPipettePanel()
         self.addPlatePanel()
-        self.createBottomMenu()
 
-        # Initialize log handler
-        self.lh = log_helper.LogHandler()
-        logger = logging.getLogger()
-        logger.addHandler(self.lh)
+        # Initialize buttons along bottom of application
+        self.createBottomMenu()
 
     # Adds the File and Help menus
     def addMenubar(self):
@@ -40,13 +30,13 @@ class ProtocolGenerator:
 
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label='Generate Protocol', command=self.save)
-        filemenu.add_command(label='Edit Labware', command=self.editLabware)
+        filemenu.add_command(label='Edit Labware', command=self.mh.editLabware)
         filemenu.add_command(label='Quit', command=self.quit)
         menubar.add_cascade(label='File', menu=filemenu)
 
         helpmenu = tk.Menu(menubar, tearoff=0)
-        helpmenu.add_command(label='Help', command=self.help)
-        helpmenu.add_command(label='About', command=self.about)
+        helpmenu.add_command(label='Help', command=self.mh.help)
+        helpmenu.add_command(label='About', command=self.mh.about)
         menubar.add_cascade(label='Help', menu=helpmenu)
 
         self.window.config(menu=menubar)
@@ -88,58 +78,11 @@ class ProtocolGenerator:
 
     # Outputs the protocol to a file
     def save(self):
-        pw = protocol_writer.ProtocolWriter()
-
-        try:
-            # Read in data from each input panel. Throws an exception if
-            # invalid data is encountered
-            for ip in self.input_panels:
-                pw.addData(ip.getData())
-
-            if ofile := filedialog.asksaveasfilename(title='Save Protocol'):
-                pw.saveOutput(ofile)
-
-                if self.lh.text:
-                    log_helper.WarningMessageBox(self.window, self.lh.text)
-                self.quit()
-            else:
-                self.lh.clear()
-                self.window.focus()
-        except Exception as e:
-            traceback.print_exc()
-            messagebox.showerror(title='Error', message=e)
-            self.lh.clear()
-            self.window.focus()
-
-    # Open the labware.ini file for editing
-    def editLabware(self):
-        if not os.path.exists('./labware.ini'):
-            cfg = config.Configuration('./labware.ini')
-            cfg.writeFile('./labware.ini')
-
-        OS = system().lower()
-        if 'windows' in OS:
-            opener = 'start'
-        elif 'osx' in OS or 'darwin' in OS:
-            opener = 'open'
-        else:
-            opener = 'xdg-open'
-        subprocess.run(opener + ' labware.ini', shell=True)
-        msg = 'Please restart the protocol generator after editing any labware'
-        messagebox.showinfo(title='Labware', message=msg)
+        data = []
+        for ip in self.input_panels:
+            data.append(ip.getData())
+        self.mh.save(self.window, *data)
 
     # Exit the application
     def quit(self, event=None):
         self.window.destroy()
-
-    # Open the home page on github
-    def help(self):
-        url = 'https://github.com/olichen/ot2_protocol_generator#readme'
-        webbrowser.open(url)
-
-    # Pop open an about box
-    def about(self):
-        msg = ('Version 1.0\n'
-               'Copyright (c) 2020 Oliver Chen\n'
-               'https://github.com/olichen/ot2_protocol_generator')
-        messagebox.showinfo(title='About', message=msg)
