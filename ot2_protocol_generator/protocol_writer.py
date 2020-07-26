d517c8c0bebf33866be391ff018229c839077095
Oliver Chen <olichen@ucdavis.edu>
2020-07-26 14:36:23 -0700

    refactor

--- ot2_protocol_generator/protocol_writer.py
+++ ot2_protocol_generator/protocol_writer.py
@@ -6,99 +6,62 @@ from .helpers import csv_helper
 # Class that handles receiving/validating data and outputting the protocol
 class ProtocolWriter:
     def __init__(self):
-        self.data = []
-        self.csv_data = []
-        self.pipette_type = ''
+        self.pipette_data = None
+        self.plate_data = []
+        self.plate_csv = []
         self.fh = format_helper.FormatHelper()
 
     # Add another source of data (either pipette or plate data)
     def addData(self, data):
-        self.data.append(data)
-
-        # Tries to process the data; raises an exception with invalid data
         if data.data_type == 'pipette':
-            self.addPipetteData(data)
+            self.pipette_data = data
         elif data.data_type == 'plate':
-            self.addPlateData(data)
-        else:
-            err_str = "Invalid data type. We shouldn't get here"
-            raise Exception(err_str)
+            self.plate_data.append(data)
 
-    # Updates the pipette type and appends an empty string to csv_data
-    def addPipetteData(self, data):
-        if data.isMulti():
-            self.pipette_type = 'multi'
-        else:
-            self.pipette_type = 'single'
-        self.csv_data.append('')
+            # Add csv data. Pre-process multi-head transfer data
+            csv_data = csv_helper.CSVReader(data.csv_file_loc)
+            if self.pipette_data.isMulti():
+                csv_data = mth.MultiTransferHelper(csv_data.volumes)
+            self.plate_csv.append(csv_data)
 
-    # Processes and appends the CSV data
-    def addPlateData(self, data):
-        # Read in the csv data
-        csv_data = csv_helper.CSVReader(data.csv_file_loc)
-
-        # Pre-process the multi-head transfer data
-        if self.pipette_type == 'single':
-            pass
-        elif self.pipette_type == 'multi':
-            csv_data = mth.MultiTransferHelper(csv_data.volumes)
-        else:
-            err_str = "Invalid pipette type. We shouldn't ever get here"
-            raise Exception(err_str)
-        self.csv_data.append(csv_data)
-
-    # Checks whether we are trying to do a single transfer or a multi transfer
+    # Open the output file and write everything
     def saveOutput(self, output_file):
         with open(output_file, 'w') as f:
-            # Write the header to the output file
             f.write(self.fh.getHeader())
-
-            # Write all the tip racks to the output file for the pipettes
             self.outputTipRacks(f)
-
-            # Write the pipette data to the output file
             self.outputPipetteData(f)
-
-            # Iterate through all the received data
             self.outputTransferData(f)
 
     # Iterate through all the input data and write the tip rack definitions
     def outputTipRacks(self, f):
-        for data in self.data:
-            if data.data_type == 'plate':
-                name = data.tip_rack_name
-                loc = data.tip_rack_loc
-                f.write(self.fh.getTipRack(name, loc))
+        for data in self.plate_data:
+            name = data.tip_rack_name
+            loc = data.tip_rack_loc
+            f.write(self.fh.getTipRack(name, loc))
 
-    # Iterate through all the input data and write the pipette definition
+    # Write the pipette definition
     def outputPipetteData(self, f):
-        for data in self.data:
-            if data.data_type == 'pipette':
-                name = data.pipette_name
-                loc = data.pipette_loc
-                f.write(self.fh.getPipette(name, loc))
+        name = self.pipette_data.pipette_name
+        loc = self.pipette_data.pipette_loc
+        f.write(self.fh.getPipette(name, loc))
 
     # Iterate through all the input data and write the plate definitions
     # followed by all the transfers
     def outputTransferData(self, f):
-        for i, data in enumerate(self.data):
-            if data.data_type == 'plate':
-                # Write the plate definitions
-                name = data.src_plate_name
-                loc = data.src_plate_loc
-                f.write(self.fh.getSrcPlate(name, loc))
-                name = data.dest_plate_name
-                loc = data.dest_plate_loc
-                f.write(self.fh.getDestPlate(name, loc))
+        for data, csv in zip(self.plate_data, self.plate_csv):
+            # Write the plate definitions
+            name = data.src_plate_name
+            loc = data.src_plate_loc
+            f.write(self.fh.getSrcPlate(name, loc))
+            name = data.dest_plate_name
+            loc = data.dest_plate_loc
+            f.write(self.fh.getDestPlate(name, loc))
 
-                # Write transfers
-                if self.pipette_type == 'multi':
-                    self.outputMultiTransfers(f, self.csv_data[i])
-                elif self.pipette_type == 'single':
-                    self.outputSingleTransfers(f, self.csv_data[i])
-                else:
-                    err_str = "Invalid pipette type. We shouldn't get here"
-                    raise Exception(err_str)
+            # Write transfers
+            if self.pipette_data.isMulti():
+                self.outputMultiTransfers(f, csv)
+            else:
+                self.outputSingleTransfers(f, csv)
 
     # Writes all the transfers by well to the output protocol file
     def outputSingleTransfers(self, f, csv_data):
@@ -110,7 +73,5 @@ class ProtocolWriter:
         # Iterates and writes the transfers by column to the output protocol
         # file if it receives a non-empty column
         for i in range(12):
-            vol = csv_data.col_volumes[i]
-            if vol:
-                col = i + 1
-                f.write(self.fh.getMultiTransfer(vol, col))
+            if vol := csv_data.col_volumes[i]:
+                f.write(self.fh.getMultiTransfer(vol, i+1))
